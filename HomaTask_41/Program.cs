141a682f324b2daa043317566e235666a8840200
Azam Amonov <azamamonov555@gmail.com>
2023-09-12 16:52:02 +0500

    Person record

--- HomaTask_41/Program.cs
+++ HomaTask_41/Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+using HomaTask_41.N36_HT1;
+
+var person = new Person("John", "Doe");
+var employee = new Employee("Anna", "doe", "example@example.com", "qwerty");
+
+Console.WriteLine(person);
+Console.WriteLine(employee);
