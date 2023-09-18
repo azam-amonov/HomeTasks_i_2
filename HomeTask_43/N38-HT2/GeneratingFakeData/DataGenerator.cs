e124263936d93f27435f2d9b430b5ea44e171cb3
Azam Amonov <azamamonov555@gmail.com>
2023-09-18 12:31:03 +0500

    N39 HT_1

--- HomeTask_43/N38-HT2/GeneratingFakeData/DataGenerator.cs
+++ HomeTask_43/N38-HT2/GeneratingFakeData/DataGenerator.cs
@@ -51,4 +51,5 @@ public class DataGenerator
         return _blogPosts = fakeBlogPosts.Generate(countOfFakeData);
     }
     
+    
 }
---
+++ HomeTask_44/N39-HT1/Model/User.cs
@@ -0,0 +1,6 @@
+namespace HomeTask_44.N39_HT1.Model;
+
+public class User
+{
+    
+}
